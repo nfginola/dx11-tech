5f97f2273e4f591dbada7df529858f36b930012d
Nadhif Ginola <nfginola@gmail.com>
2022-01-25 09:58:17 +0100

    remove unused files in fmt

--- dx11-tech/vendor/fmt-8.1.1/test/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Github/dx11-tech/dx11-tech/vendor/fmt-8.1.1/test/gtest
-# Build directory: D:/Github/dx11-tech/dx11-tech/vendor/fmt-8.1.1/test/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
