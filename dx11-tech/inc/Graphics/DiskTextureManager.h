63dc6b40365715d95886792ea8e46336513be6a1
Nadhif Ginola <nfginola@gmail.com>
2022-02-06 21:36:06 +0100

    .

--- dx11-tech/inc/Graphics/DiskTextureManager.h
+++ dx11-tech/inc/Graphics/DiskTextureManager.h
@@ -20,7 +20,7 @@ private:
 	std::map<void*, GPUTexture> m_textures;
 
 	/*
-		Removing a texture requires iterating over this map to find the corresponding internal resource..
+		Removing a texture requires iterating over this map to find the corresponding internal resource.. worst case O(n)
 		We'll find a better solution if we NEED it.
 	*/
 	std::map<std::filesystem::path, void*> m_path_mapper;
